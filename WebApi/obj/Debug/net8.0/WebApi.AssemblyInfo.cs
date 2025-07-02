e1cbe8866cca767941aada29103419cf4073fc8a
suleymannmeral <mrlslymn02@gmail.com>
2025-07-02 10:48:42 +0300

    Rate Limiting

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f26f7b0eff9c187ab43534d9f72ff86cb529028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fc9e9e8976becfdaaa38b2fb1532e5978338e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
